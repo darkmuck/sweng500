73e0b0c3f56e71d9642b5c18f0acaa8b55fdfe79
Kevin <scheibk@gmail.com>
2013-02-09 18:34:10 -0500

    Add simpletest and test cases

---
+++ code/sweng500app/app/tests/cases/controllers/users_controller.test.php
@@ -0,0 +1,15 @@
+<?php
+/* SWENG500 - Team 3
+ * William DiStefano, Dawn Viscuso, Kevin Scheib, David Singer
+ *
+ * File: users_controller.test.php
+ * Description: 
+ * Created: Feb 9, 2013
+ * Modified: Feb 9, 2013 1:23:31 PM
+ * Modified By: Kevin Scheib
+*/
+
+class UserControllerTest extends CakeTestCase {
+	
+}
+?>
