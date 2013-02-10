1b144e43d554ca0e269ef5c24ff5892462cef840
Kevin <scheibk@gmail.com>
2013-02-09 21:40:08 -0500

    Update controller test

--- code/sweng500app/app/tests/cases/controllers/users_controller.test.php
+++ code/sweng500app/app/tests/cases/controllers/users_controller.test.php
@@ -9,13 +9,55 @@
  * Modified By: Kevin Scheib
 */
 
+/*This class is to override some of the functionality that breaks testing of controllers
+see http://mark-story.com/posts/view/testing-cakephp-controllers-the-hard-way */
+
+App::import('Controller', 'Users');
+class TestUserController extends UsersController {
+	var $name = 'Users';
+ 
+    var $autoRender = false;
+ 
+    function redirect($url, $status = null, $exit = true) {
+        $this->redirectUrl = $url;
+    }
+ 
+    function render($action = null, $layout = null, $file = null) {
+        $this->renderedAction = $action;
+    }
+ 
+    function _stop($status = 0) {
+        $this->stopped = $status;
+    }
+}
+
+
 class UserControllerTest extends CakeTestCase {
+	
+	function startTest() {
+		$this->TestUserController = new TestUserController();
+		$this->TestUserController->constructClasses();
+		$this->TestUserController->Component->initialize($this->TestUserController);
+	}
+	
+	function endTest() {
+		unset($this->TestUserController);
+		ClassRegistry::flush();
+	}
+	
 	function testLogin() {
-		$data = array('username' => 'tester', 'password' => 'tester');
-		$result = $this->testAction('/users/login', array('method'=> 'post', 
-					'return' => 'result', 
-					'data' => $data));
-		debug($result);
+		$this->TestUserController->Auth->data = array ('User' => array('username' => 'tester', 
+				'password' => 'tester'));
+		
+		$this->TestUserController->data = array('username' => 'tester', 'password' => 'tester');
+		
+		$this->TestUserController->params = Router::parse('/users/login');
+	    $this->TestUserController->beforeFilter();
+
+	    $this->TestUserController->login();
+	    
+	    //assert the url
+	    $this->assertEqual($this->TestUserController->redirectUrl, array('action' => 'start'));
 	}
 }
 ?>
