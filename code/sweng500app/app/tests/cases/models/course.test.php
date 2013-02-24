5be1ca39ad6930bf75fd62cf8ed7fc5a42458aa8
David Singer <dlsinger@ymail.com>
2013-02-24 14:19:20 -0700

    courses model test

---
+++ code/sweng500app/app/tests/cases/models/course.test.php
@@ -0,0 +1,19 @@
+<?php
+/* SWENG500 - Team 3
+ * William DiStefano, Dawn Viscuso, Kevin Scheib, David Singer
+ *
+ * File: course.test.php
+ * Description: 
+ * Created: 2013-02-23
+ * Modified: 2013-02-23 15:36
+ * Modified By: David Singer
+*/
+
+App::import('Model','course');
+	
+class CourseTestCase extends CakeTestCase
+{
+	
+}
+
+?>
