f25360c60b81dbec063c9b338574e4eec9b77d72
Kevin <scheibk@gmail.com>
2013-03-08 11:46:36 -0500

    add quiz submission models

---
+++ code/sweng500app/app/models/submittedanswer.php
@@ -0,0 +1,11 @@
+<?php
+/* SWENG500 - Team 3
+ * William DiStefano, Dawn Viscuso, Kevin Scheib, David Singer
+ *
+ * File: submittedanswer.php
+ * Description: 
+ * Created: Mar 8, 2013
+ * Modified: Mar 8, 2013 10:56:22 AM
+ * Modified By: Kevin Scheib
+*/
+?>
